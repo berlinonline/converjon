fd0e40f8d2c9b08643ab4996a31cfcc51f701636
Leon Weidauer <leon.weidauer@googlemail.com>
2012-08-01 15:43:51 +0200

    Removed command line options in favor of configuration.

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ var config = require('config');
 
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 
-var applyDefaults = require('./defaults');
-var args = require('argsparser').parse();
-args = applyDefaults(args);
-
-if ("help" in args && args.help)
-{
-    console.log("\nUsage:");
-    console.log("\n-p <port> Sets the server port to <port>, defaults to 8000");
-    console.log("\n-h/--help Displays this message.");
-    console.log("\n");
-    process.exit();
-}
-
 require('./lib/preparations');
 
 var connect = require('connect');
@@ -29,7 +16,5 @@ server.use(require('./lib/requestParser')());
 server.use(require('./lib/urlChecker')());
 server.use(require('./lib/imageFetcher')());
 
-server.listen(args.port);
-
-console.log('Server running on port ' + args.port);
+server.listen(config.server.port);
 
