db5aa6388179925a0d2e73ae7fefee66a3168a1b
Leon Weidauer <leon.weidauer@googlemail.com>
2014-08-12 15:40:59 +0200

    new cache system

--- test/download.js
+++ test/download.js
@@ -11,6 +11,8 @@ var rsvp = require("rsvp");
 var Promise = rsvp.Promise;
 
 var pathutils = require("../lib/pathutils");
+var fsutils = require("../lib/fsutils");
+var lock = require("../lib/lock");
 var config = require("../lib/config");
 config.load([
     pathutils.join([__dirname, "resources", "test_config.yml"])
@@ -42,7 +44,19 @@ module.exports = {
 
         var url1 = "http://localhost:10000/test_image_sparrow.jpg";
         var conf1 = config.get(url1);
-        get_source(url1, conf1).then(function(dir_path) {
+
+        var dir_path = conf1.cache.base_path;
+
+        fsutils.mkdirp(dir_path).then(function() {
+            return rsvp.hash({
+                meta: lock(pathutils.join([dir_path, "meta"])),
+                source: lock(pathutils.join([dir_path, "source"]))
+            });
+        }).then(function(locks){
+            return get_source(url1, locks, conf1);
+        }).then(function(result) {
+            result.locks.meta();
+            result.locks.source();
             return new Promise(function(resolve, reject){
                 var path = pathutils.join([dir_path, "source"]);
                 fs.open(path, "r", function(err, file) {
@@ -68,57 +82,66 @@ module.exports = {
 
         var url1 = "http://localhost:10000/test_image_sparrow.jpg";
         var conf1 = config.get(url1);
-        var d1 = new Promise(function(resolve, reject) {
-            get_source(url1, conf1).then(function(dir_path) {
-                return new Promise(function(resolve, reject){
-                    //does the source image file exist?
-                    var path = pathutils.join([dir_path, "source"]);
-                    fs.open(path, "r", function(err, file) {
-                        test.strictEqual(err, null);
-                        resolve(dir_path);
-                    });
+
+        var dir_path = conf1.cache.base_path;
+
+        var d1 = fsutils.mkdirp(dir_path).then(function() {
+            return rsvp.hash({
+                meta: lock(pathutils.join([dir_path, "meta1"])),
+                source: lock(pathutils.join([dir_path, "source1"]))
+            });
+        }).then(function(locks){
+            return get_source(url1, locks, conf1);
+        }).then(function(result) {
+            result.locks.meta();
+            result.locks.source();
+            return new Promise(function(resolve, reject){
+                var path = pathutils.join([dir_path, "source1"]);
+                fs.open(path, "r", function(err, file) {
+                    test.strictEqual(err, null);
+                    resolve(dir_path);
                 });
-            }).then(function(dir_path){
-                return new Promise(function(resolve, reject){
-                    //does the metadata file exist?
-                    var path = pathutils.join([dir_path, "meta"]);
-                    fs.open(path, "r", function(err, file) {
-                        test.strictEqual(err, null);
-                        resolve(dir_path);
-                    });
+            });
+        }).then(function(dir_path){
+            return new Promise(function(resolve, reject){
+                var path = pathutils.join([dir_path, "meta1"]);
+                fs.open(path, "r", function(err, file) {
+                    test.strictEqual(err, null);
+                    resolve(dir_path);
                 });
-            }).then(function(){
-                resolve();
             });
         });
 
         var url2 = "http://localhost:10000/test_image_sparrow_2.jpg";
-        var conf2 = config.get(url2);
-        var d2 = new Promise(function(resolve, reject) {
-            get_source(url2, conf2).then(function(dir_path) {
-                return new Promise(function(resolve, reject){
-                    //does the source image file exist?
-                    var path = pathutils.join([dir_path, "source"]);
-                    fs.open(path, "r", function(err, file) {
-                        test.strictEqual(err, null);
-                        resolve(dir_path);
-                    });
+        var conf2 = config.get(url1);
+
+        var d2 = fsutils.mkdirp(dir_path).then(function() {
+            return rsvp.hash({
+                meta: lock(pathutils.join([dir_path, "meta2"])),
+                source: lock(pathutils.join([dir_path, "source2"]))
+            });
+        }).then(function(locks){
+            return get_source(url2, locks, conf2);
+        }).then(function(result) {
+            result.locks.meta();
+            result.locks.source();
+            return new Promise(function(resolve, reject){
+                var path = pathutils.join([dir_path, "source2"]);
+                fs.open(path, "r", function(err, file) {
+                    test.strictEqual(err, null);
+                    resolve(dir_path);
                 });
-            }).then(function(dir_path){
-                return new Promise(function(resolve, reject){
-                    //does the metadata file exist?
-                    var path = pathutils.join([dir_path, "meta"]);
-                    fs.open(path, "r", function(err, file) {
-                        test.strictEqual(err, null);
-                        resolve(dir_path);
-                    });
+            });
+        }).then(function(dir_path){
+            return new Promise(function(resolve, reject){
+                var path = pathutils.join([dir_path, "meta2"]);
+                fs.open(path, "r", function(err, file) {
+                    test.strictEqual(err, null);
+                    resolve(dir_path);
                 });
-            }).then(function(){
-                resolve();
             });
         });
 
-        //wait for the two test promises to resolve, then wrap it up.
         rsvp.all([d1, d2]).then(function(){
             test.done();
         });
@@ -129,12 +152,20 @@ module.exports = {
 
         var url1 = "http://localhost:10000/non_existing.jpg";
         var conf1 = config.get(url1);
-        get_source(url1, conf1).then(
+
+        rsvp.hash({
+            meta: lock("meta"),
+            source: lock("source"),
+        }).then(function(locks) {
+            return get_source(url1, locks, conf1);
+        }).then(
             function() {
                 test.equal("this should not happen", "ever!");
             },
-            function(err) {
-                test.strictEqual(err.message, "HTTP 404");
+            function(fail) {
+                test.strictEqual(fail.error.message, "HTTP 404");
+                fail.locks.meta();
+                fail.locks.source();
                 test.done();
             }
         );
@@ -145,12 +176,19 @@ module.exports = {
 
         var url1 = "http://foobar/";
         var conf1 = config.get(url1);
-        get_source(url1, conf1).then(
+        rsvp.hash({
+            meta: lock("meta"),
+            source: lock("source"),
+        }).then(function(locks) {
+            return get_source(url1, locks, conf1);
+        }).then(
             function(foo) {
                 test.equal("this should not happen", "ever!");
             },
-            function(err) {
-                test.strictEqual(err.message, "getaddrinfo ENOTFOUND");
+            function(fail) {
+                fail.locks.meta();
+                fail.locks.source();
+                test.strictEqual(fail.error.message, "getaddrinfo ENOTFOUND");
                 test.done();
             }
         );
