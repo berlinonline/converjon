4cb07cbb23d339169d3b664b1889869f5285da2a
Leon Weidauer <leon@lnwdr.de>
2015-05-27 17:14:31 +0200

    fixed config merging and matching. #92

--- test/config_merged.js
+++ test/config_merged.js
@@ -11,23 +11,28 @@ module.exports = {
 
     setUp: function(cb) {
         config.load([
+            pathutils.join([__dirname, "resources", "merge_test_config.yml"]),
             pathutils.join([__dirname, "resources", "merge_test_config.json"])
         ]);
-
-
         cb();
     },
 
-    testMergedJsonConfig: function (test) {
-        test.expect(3);
+    testMergedConfig: function (test) {
+        test.expect(5);
 
         var cfg = config.get("http://localhost:8001/foobar" );
 
         test.strictEqual(cfg.server.port, 8000);
         test.strictEqual(cfg.urls[0], "http://localhost:8001*");
         test.strictEqual(cfg.cache.base_path, "cache");
+        test.strictEqual(cfg.foo.bar, 7);
+
+        var cfg2 = config.get("http://example.org/something");
+        test.strictEqual(cfg2.foo.bar, 5);
 
         test.done();
     }
+
 };
+
 
