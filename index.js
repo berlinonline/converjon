989b4701203f98ec4f1401a978bf2dda686b6b42
Steffen Gransow <graste@mivesto.de>
2015-05-31 18:36:11 +0200

    add more package.json details and empty index.js
    refs #95

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * At the moment you can't use converjon via require('converjon').
+ *
+ * Please use the commandline client or standalone server version.
+ *
+ * @see bin/converjon.js
+ * @see bin/converion-cli.js
+ */
+exports.init = function () {}
